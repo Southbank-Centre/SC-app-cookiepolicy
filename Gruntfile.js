b5af83ea4b56a837510d59a344572285672da4b4
mattc-cogapp <mattc@cogapp.com>
2015-05-12 13:31:17 +0100

    Added copy tasks. Added tests for post-build component

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,18 +26,77 @@ module.exports = function (grunt) {
       unit: {
         configFile: 'karma.conf.js',
         singleRun: true
+      },
+      dist: {
+        configFile: 'karma.conf.js',
+        singleRun: true,
+        files: [
+          {
+            src: [
+              'bower_components/angular/angular.min.js',
+              'bower_components/angular-cookies/angular-cookies.min.js',
+              'bower_components/angular-mocks/angular-mocks.js',
+              'dist/*.js',
+              'test/*.js',
+              'dist/**/*.html'
+            ]
+          }
+        ],
+        ngHtml2JsPreprocessor: {
+          stripPrefix: 'dist/',
+          moduleName: 'sc.app.cookiepolicy.templates'
+        }
+
+      }
+    },
+
+    uglify: {
+      dist: {
+        options: {
+          sourceMap: true,
+          sourceMapName: 'dist/directive.cookiepolicy.min.js.map'
+        },
+        files: {
+          'dist/directive.cookiepolicy.min.js': 'src/directive.cookiepolicy.js'
+        }
+      }
+    },
+
+    htmlmin: {
+      dist: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {
+          'dist/templates/directive.cookiepolicy.tpl.html': 'src/templates/directive.cookiepolicy.tpl.html'
+        }
+      }
+    },
+
+    copy: {
+      dist: {
+        src: './src/directive.cookiepolicy.js',
+        dest: './dist/directive.cookiepolicy.js'
       }
     }
+
   });
 
 
   grunt.registerTask('build', [
+    'jshint',
+    'karma:unit',
+    'copy:dist',
+    'uglify',
+    'htmlmin',
+    'karma:dist'
 
   ]);
 
   grunt.registerTask('test', [
     'jshint',
-    'karma'
+    'karma:unit'
   ]);
 
   grunt.registerTask('default', ['test']);
