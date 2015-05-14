0290a8c46bf19a15a94a4db7ed91ddf1530511f5
mattc-cogapp <mattc@cogapp.com>
2015-05-14 11:04:33 +0100

    Added some comments

--- src/directive.cookiepolicy.js
+++ src/directive.cookiepolicy.js
@@ -2,10 +2,10 @@
 
 /**
  * @ngdoc directive
- * @name sc.cookiepolicy.directive:cookiepolicy
+ * @name SC-app-cookiepolicy.directive:scCookiePolicy
  * @description
  * Show a cookie policy notice.
- * @example <div acme-shared-spinner></div>
+ * @example <div sc-cookie-policy irights-page="irightsTest" cookie-page="cookiePageTest"></div>
  */
 angular
   .module('SC-app-cookiepolicy', [])
@@ -26,6 +26,7 @@ function scCookiePolicy($cookieStore) {
 
   function link($scope) {
 
+    // Check if the acceptance cookie has been set.
     $scope.checkCookiePolicyAccepted = function(){
       var policyAccepted = $cookieStore.get('scCookiePolicyAccepted');
 
@@ -38,13 +39,13 @@ function scCookiePolicy($cookieStore) {
       return false;
     };
 
+    // Set the policy cookie. This is called in an nc-click() directive in the directive template.
     $scope.setPolicyCookie = function(){
       $cookieStore.put('scCookiePolicyAccepted', true);
       $scope.scCookiePolicyAccepted = true;
     };
 
     $scope.scCookiePolicyAccepted = $scope.checkCookiePolicyAccepted();
-
   }
 
   return ddo;
